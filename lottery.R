f0e9a45402322eda6ca7d10b40df53877f815c5e
Tegan <tlmarianchuk@gmail.com>
2019-09-11 16:02:36 -0400

    Added shortened code
    shorter code using "rbinom" to generate 100 lotto tix but w/shorter code

--- lottery.R
+++ lottery.R
@@ -13,18 +13,26 @@ library(stats)
 
 num_of_coin_flips <- 10 
 num_of_generated_tix <- 100 #generate 100 tix
+#generate_one_lotto_ticket <- for (i in 1:num_of_coin_flips){
+  #ticket_nums[i] <- if (runif(1) < 0.5) 1 else 0
+#}
 
 ticket_nums<- array(0,num_of_coin_flips)
+#generate_one_lotto_ticket
 for (i in 1:num_of_coin_flips){
   ticket_nums[i] <- if (runif(1) < 0.5) 1 else 0
-  print(ticket_nums)
 }
-
+#  print(ticket_nums)
 for (j in 1:num_of_generated_tix){
-  ticket_nums <- array(0,10)
+  #generate_one_lotto_ticket
+ # ticket_nums <- array(0,10)
   for (i in 1:num_of_coin_flips){
     ticket_nums[i] <- if (runif(1) < 0.5) 1 else 0
   }
   print(ticket_nums)
 }
 
+#shorter code to do same as above
+for (j in 1:100){
+  ticket<-rbinom(10,1,0.1)
+  print(ticket)}
