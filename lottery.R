f0513577ec9c63de8ef4184357d314fe5229a6b8
Tegan <tlmarianchuk@gmail.com>
2019-09-11 15:13:43 -0400

    changed variable names
    making script more readable

--- lottery.R
+++ lottery.R
@@ -11,15 +11,20 @@ library(stats)
 
 # Small edit! from class 1!
 
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+num_of_coin_flips <- 10 
+num_of_generated_tix <- 100 #generate 100 tix
+
+ticket_nums<- array(0,num_of_coin_flips)
+for (i in 1:num_of_coin_flips){
+  ticket_nums[i] <- if (runif(1) < 0.5) 1 else 0
+  print(ticket_nums)
 }
 
-for (j in 1:100){
-  arr <- array(0,10)
-  for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+for (j in 1:num_of_generated_tix){
+  ticket_nums <- array(0,10)
+  for (i in 1:num_of_coin_flips){
+    ticket_nums[i] <- if (runif(1) < 0.5) 1 else 0
   }
-  print(arr)
+  print(ticket_nums)
 }
+
